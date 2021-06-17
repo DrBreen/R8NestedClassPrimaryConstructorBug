38ea9201b6f1d0104cb37a48ddf1157597c59a25
Alexander Leontev <alexander.leontev@share-now.com>
2021-06-17 10:38:45 +0200

    Add printing of Metadata

--- app/src/main/java/com/woodblockwithoutco/r8crash/MainActivity.kt
+++ app/src/main/java/com/woodblockwithoutco/r8crash/MainActivity.kt
@@ -3,6 +3,7 @@ package com.woodblockwithoutco.r8crash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.primaryConstructor
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
         val clazz = SomeObject::class
         Log.d("r8Bug", "Class: $clazz")
+
+        val outerMetadata = SomeObject::class.java.annotations.firstOrNull { it is Metadata }
+        Log.d("r8Bug", "Outer metadata present: ${outerMetadata != null}")
+
+        val innerMetadata = SomeObject.Inner::class.java.annotations.firstOrNull { it is Metadata }
+        Log.d("r8Bug", "Inner metadata present: ${innerMetadata != null}")
 
         val primaryConstructorInner = SomeObject.Inner::class.primaryConstructor
         Log.d("r8Bug", "Primary constructor: $primaryConstructorInner")
