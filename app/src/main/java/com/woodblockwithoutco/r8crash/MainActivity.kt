28d05b968ab92077d3622760de752308b4d45a06
Alexander Leontev <alexander.leontev@share-now.com>
2021-06-21 11:27:43 +0200

    Show more gritty details of Kotlin inner workings

--- app/src/main/java/com/woodblockwithoutco/r8crash/MainActivity.kt
+++ app/src/main/java/com/woodblockwithoutco/r8crash/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.internal.impl.metadata.deserialization.Flags
+import kotlin.reflect.jvm.internal.impl.metadata.jvm.deserialization.JvmProtoBufUtil
+import kotlin.reflect.jvm.internal.impl.serialization.deserialization.ProtoEnumFlags
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,17 +18,39 @@ class MainActivity : AppCompatActivity() {
         val clazz = SomeObject::class
         Log.d("r8Bug", "Class: $clazz")
 
-        val outerMetadata = SomeObject::class.java.annotations.firstOrNull { it is Metadata }
+        val outerMetadata = SomeObject::class.java.annotations.firstOrNull { it is Metadata } as? Metadata
         Log.d("r8Bug", "Outer metadata present: ${outerMetadata != null}")
 
-        val innerMetadata = SomeObject.Inner::class.java.annotations.firstOrNull { it is Metadata }
+        val innerMetadata = SomeObject.Inner::class.java.annotations.firstOrNull { it is Metadata } as? Metadata
         Log.d("r8Bug", "Inner metadata present: ${innerMetadata != null}")
 
         val primaryConstructorInner = SomeObject.Inner::class.primaryConstructor
-        Log.d("r8Bug", "Primary constructor: $primaryConstructorInner")
+        Log.d("r8Bug", "Primary constructor inner: $primaryConstructorInner")
 
         val primaryConstructorOuter = Outer::class.primaryConstructor
-        Log.d("r8Bug", "Primary constructor: $primaryConstructorOuter")
+        Log.d("r8Bug", "Primary constructor outer: $primaryConstructorOuter")
+
+        if (innerMetadata != null) {
+            val protoInnerClass = JvmProtoBufUtil.readClassDataFrom(innerMetadata.data1, innerMetadata.data2).second
+            Log.d("r8Bug", "Constructor flags: ${protoInnerClass.constructorList.first().flags}")
+
+            val primaryConstructor = protoInnerClass.constructorList.firstOrNull { !Flags.IS_SECONDARY.get(it.flags) }
+            Log.d("r8Bug", "Proto primary constructor: $primaryConstructor")
+
+            val constructor = SomeObject.Inner::class.constructors.first()
+            Log.d("r8Bug", "Inner constructor: ${constructor.javaClass}")
+
+            val kFunctionImplClazz = Class.forName("kotlin.reflect.jvm.internal.KFunctionImpl")
+            val constructorDescriptorMethod = kFunctionImplClazz.methods.first { it.name.contains("getDescriptor") }
+
+            val descriptor = constructorDescriptorMethod.invoke(constructor)
+            Log.d("r8Bug", "Descriptor: $descriptor, descriptor class: ${descriptor.javaClass}")
+
+            val isPrimaryMethod = descriptor.javaClass.methods.first { it.name.contains("isPrimary") }
+            val isPrimary = isPrimaryMethod.invoke(descriptor)
+            Log.d("r8Bug", "Is descriptor primary: $isPrimary")
+
+        }
     }
 
 }
